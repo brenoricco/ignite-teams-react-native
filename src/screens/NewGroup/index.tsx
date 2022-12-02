fe40e8e536c6ea8122d199229b6384a4e5c31a44
brenoricco <brenoriccodev@gmail.com>
2022-12-01 21:49:40 -0300

    Criando component Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
@@ -9,12 +10,18 @@ export function NewGroup() {
             <Header showBackButton />
             <Content>
                 <Icon />
+
                 <Highlight
                     title="Nova turma"
                     subtitle="crie a turma para adicionar as pessoas"
                 />
 
-                <Button title="Criar" />
+                <Input />
+
+                <Button 
+                    title="Criar" 
+                    style={{ marginTop: 20 }}
+                />
             </Content>
         </Container>
     );
