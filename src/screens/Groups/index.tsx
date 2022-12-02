12d3401cf8e2344591614d905ec2d113ac74d644
brenoricco <brenoriccodev@gmail.com>
2022-12-01 21:09:13 -0300

    Criação componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,14 @@
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
+    const [groups, setGroups] = useState(['Galera da Rocket']);
+
     return (
         <Container>
             <Header />
@@ -13,7 +18,23 @@ export function Groups() {
                 subtitle="joque com a sua turma"
             />
 
-            <GroupCard title="Galera do Ignite" />
+            <FlatList 
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard 
+                        title={item} 
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message="Que tal cadastrar a primeira turma?" 
+                    />
+                )}
+            />
+
+            
         </Container>
     );
 }
